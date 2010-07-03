45b9fb58e2a38c8e8d8864849f5b8413103076a5
sharat.chikkerur <sharat.chikkerur@f88e77bb-55f1-c420-0acf-413071fbb9f3>
2010-07-03 02:09:11 +0000

    updated version. Previous checkin incorrect

--- attention/test_invariance.m
+++ attention/test_invariance.m
@@ -1,108 +1,146 @@
 %--------------------------------------------------
-%test_invariance
-%shows the spatial invariance property of the 
-%bayesian attention model.
-
+%demonstrates the translation invariance of the 
+%bayesian model.
 %sharat@mit.edu
-addpath(genpath(fullfile('third_party','BNT')));
+addpath(genpath('third_party/BNT'));
 addpath(genpath('cbcl-model-matlab'));
-warning('off','all')
-%PARAMETERS
-L = 1; F=2; I_start=2;
-SZ= 5; N=SZ*SZ; sigma=.01;
-dag   = zeros(I_start+N);
-DELTA = 0;
-NDIR  = 4;
 EPS=0.01;
 EPS2=0.01;
 
+SZ= 5; N=SZ*SZ; 
+DELTA = 0;
+NDIR  = 4;
+L = 1; F_start=1; C_start=F_start+NDIR;
+dag   = zeros(C_start+NDIR);
 %---------------------------
-%connectiveity
-for i=1:N
-    dag(L,I_start+i)=1;
-    dag(F,I_start+i)=1;
+%connectivity
+for i=1:NDIR
+    dag(L,C_start+i)        =1;
+    dag(F_start+i,C_start+i)=1;
 end;
 
-bnet = mk_bnet(dag,[N NDIR+1 (NDIR+1)*ones(1,N)],'discrete',[F L I_start+[1:N]]);
-
+bnet = mk_bnet(dag,[N ones(1,NDIR)*2 ones(1,NDIR)*(N+1)],'discrete',[L F_start+[1:NDIR] C_start+[1:NDIR]]);
 %---------------------------------------------
-%connectivity 
-bnet.CPD{F}=tabular_CPD(bnet,F,'CPT','unif');
+%test 
 bnet.CPD{L}=tabular_CPD(bnet,L,'CPT','unif');
-for c=1:N
-     tbl    =zeros(N,NDIR+1,NDIR+1);
-     for l=1:N
-		    for oval=1:NDIR+1
-			    if(l==c)
-                    for cval=1:NDIR+1
-            		   tbl(l,oval,cval) = (cval==oval)*(1-EPS)+EPS;
-				    end;
-                    tbl(l,oval,:)=tbl(l,oval,:)/sum(tbl(l,oval,:));
-                else
-                    for cval=1:NDIR+1
-                        tbl(l,oval,cval)= (cval==(NDIR+1))*(1-EPS)+EPS;
-                    end;
-                    tbl(l,oval,:)=tbl(l,oval,:)/sum(tbl(l,oval,:));
-                end;
-			end;%oval
-     end;%l
-     bnet.CPD{I_start+c}=tabular_CPD(bnet,I_start+c,tbl);
+for f=1:NDIR
+  bnet.CPD{F_start+f}=tabular_CPD(bnet,F_start+f,'CPT','unif');
+  tbl    =zeros(N,2,N+1);
+  for l=1:N
+	  for fval=1:2
+            for cval=1:N+1
+			   if(fval==1)
+				 val= (1-EPS)*(cval==l)+EPS;
+			   else
+				 val= (1-EPS)*(cval==N+1)+EPS;
+			   end;
+			   tbl(l,fval,cval)=val;%the last part is
+                                    %important for bottom up
+			end;
+			tbl(l,fval,:)=tbl(l,fval,:)/sum(tbl(l,fval,:)); 
+	  end;	   
+  end;		  
+  bnet.CPD{C_start+f}=tabular_CPD(bnet,C_start+f,'CPT',tbl);
 end;
 %-----------------------------------------------------
 %generate stimulus
-cellSize      =  13;
-RF            =  13;
+RF            =  17;
 
 %------------------------------------------------------
 %get prior
+or            = {};
+pL            = {};
+%change the position of the stimuli
+or{1}         =  zeros(SZ); or{1}(2,2)=1; 
+or{2}         =  zeros(SZ); or{2}(3,3)=1; 
+or{3}         =  zeros(SZ);  or{3}(4,4)=1;
+%set location prior to be uniform
+pL{1}         =  ones(SZ,SZ); pL{1}=pL{1}/sum(pL{1}(:));
+pL{2}         =  ones(SZ,SZ); pL{2}=pL{2}/sum(pL{2}(:));
+pL{3}         =  ones(SZ,SZ); pL{3}=pL{3}/sum(pL{3}(:));
+%set feature prior to be uniform
+pF{1}         = {[0.5 0.5],[0.5 0.5],[0.5 0.5],[0.5 0.5]};
+pF{2}         = {[0.5 0.5],[0.5 0.5],[0.5 0.5],[0.5 0.5]};
+pF{3}         = {[0.5 0.5],[0.5 0.5],[0.5 0.5],[0.5 0.5]};
 
-input=1;
-for xpos=[0 2 3 4]
-  for ypos=xpos
-	or       	  =  zeros(SZ); if(xpos>0) or(ypos,xpos)=1; end;
-	stim	      =  imfilter(create_stimulus(or,NDIR,RF,cellSize),fspecial('gaussian'));
-	c0            =  create_c0(stim,1,1);
+
+for input=1:3
+    %create stimulus
+	stim	      =  create_stimulus(or{input},NDIR,RF,RF);
 	gabors        =  getGabors(RF,NDIR);
-	res           =  EPS2*ones(SZ,SZ,NDIR+1);
-    %get bottom-up evidence for each position/feature combination
-	for f=1:NDIR
-		res(:,:,f)=blkproc(stim,[cellSize cellSize],@(x) sum(sum(squeeze(gabors(:,:,f)).*x)));
-        res(:,:,f)=max(res(:,:,f),0);
-	end;
+	res           =  zeros(SZ,SZ,NDIR+1);
+    for f=1:NDIR
+		res(:,:,f)=blkproc(stim,[RF RF],@(x) sum(sum(x.*gabors(:,:,f))));
+        res(:,:,f)=abs(res(:,:,f))
+    end;
 	engine  = jtree_inf_engine(bnet);
-	evidence= cell(I_start+N,1);
-	sevidence=cell(I_start+N,1);
-    %format bottom-up evidence
-	pos  =1;
-	for x=1:SZ
-	  for y=1:SZ
-        %the bottom-up evidence can be any linear/non-linear function of the filter outputs
-        %even thresholding works.
-		column                =reshape(abs(res(y,x,:)).^2,[NDIR+1,1]);
-		sevidence{I_start+pos}=column/sum(column);
-		pos =pos+1;
+	evidence= cell(C_start+NDIR,1);
+    %enter bottom-up evidence
+	sevidence=cell(C_start+NDIR,1);
+	for f=1:NDIR
+	  plane=squeeze(res(:,:,f));
+	  for l=1:N
+        %the bottom up evidence can be any non-linear function 
+        %of the filter response,the best being a theshold followed
+        %by a sigmoid. here 0.5 is the detection threshold.
+		sevidence{C_start+f}(1:N)=double(max(plane(:)-0.5,0));
 	  end;
+	  sevidence{C_start+f}(N+1)=EPS2;
 	end;
-    %location prior(uniform)
-	pL          = ones(SZ);pL(3,3)=1;
-	sevidence{L}=pL;
+    sevidence{L}=pL{input};
+    for f=1:NDIR
+        sevidence{F_start+f}=pF{input}{f};
+    end;        
 	engine = enter_evidence(engine,evidence,'soft',sevidence);
-	margF=marginal_nodes(engine,F);
+    %if you are using belief propagation engine(checked out from here), then replace
+    %engine=enter_evidence(engine,sevidence);
 	margL=marginal_nodes(engine,L);
 	loc(:,input)      =margL.T;
-	response(:,input) =margF.T;
+	for f=1:NDIR
+	  margF=marginal_nodes(engine,F_start+f);
+	  response(f,input) =margF.T(1);
+	end;
 	stimImage{input}=stim;
 	input=input+1;
 	fprintf('Finished %d\n',input);
-  end;%xpos
 end;%ypos  
 
 figure(2);
 
-for i=1:4;
-subplot(3,4,i);imagesc(stimImage{i});axis image off;grid on;
-subplot(3,4,4+i);bar(response(1:NDIR,i),'r'); set(gca,'YLim',[0 1]);grid on;axis on;grid on;
-subplot(3,4,8+i);imagesc(reshape(loc(:,i),[SZ SZ]));grid on;axis off;
+for i=1:3
+subplot(4,3,i);
+    imagesc(stimImage{i});
+    axis image off;
+    grid on;
+    p=[];   
+     for f=1:NDIR
+       p(f)=pF{i}{f}(1);
+     end;    
+subplot(5,3,3+i);
+    bar(p,'r'); 
+    set(gca,'YLim',[0 1]);
+    set(gca,'XLim',[0.5 4.5]);
+    set(gca,'XTickLabel','');
+    grid on;
+     
+subplot(5,3,6+i);
+    bar(response(1:NDIR,i),'r'); 
+    set(gca,'YLim',[0 1]);
+    set(gca,'XLim',[0.5 4.5]);
+    set(gca,'XTickLabel','');
+    grid on;
+
+subplot(5,3,9+i);
+    imagesc(reshape(pL{i},[SZ SZ]),[0 .4]);
+    grid on;
+    axis off;
+
+subplot(5,3,12+i);
+    imagesc(reshape(loc(:,i),[SZ SZ]),[0 0.4]);
+    grid on;
+    axis off;
 end;
+
 colormap('gray');
+set(gcf,'color','white');
 
