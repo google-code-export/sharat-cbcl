f467c55e07e8cc14b3e907bbd00d4d6cf8e1b5c1
sharat.chikkerur <sharat.chikkerur@f88e77bb-55f1-c420-0acf-413071fbb9f3>
2010-07-07 00:20:33 +0000

    Jul 6th

--- trunk/attention/test_popout.m
+++ trunk/attention/test_popout.m
@@ -3,8 +3,8 @@
 %sharat@mit.edu
 addpath(genpath('~/third_party/BNT'));
 addpath(genpath('~/cbcl-model-matlab'));
-EPS=0.01;
-EPS2=0.01;
+EPS=0.001;
+EPS2=0.001;
 
 SZ= 5; N=SZ*SZ; sigma=.01;
 DELTA = 0;
