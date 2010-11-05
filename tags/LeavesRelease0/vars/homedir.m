c45c7619f43b12faf37fd5d255e557659f0bd3c2
sharat.chikkerur <sharat.chikkerur@f88e77bb-55f1-c420-0acf-413071fbb9f3>
2010-11-05 01:33:44 +0000

    changed homedir

--- tags/LeavesRelease0/vars/homedir.m
+++ tags/LeavesRelease0/vars/homedir.m
@@ -3,7 +3,7 @@ function path = homedir
 if ispc
     error('not defined under windows');
 else
-    path = '/cbcl/cbcl01/sharat/sharat-cbcl/leaves/output';
+    path = '/cbcl/cbcl01/sharat/code/leaves/output';
 end
 
 return;
