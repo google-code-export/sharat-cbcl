87f348fc05b1f165cc7a4d539bac97537c988c44
sharat.chikkerur <sharat.chikkerur@f88e77bb-55f1-c420-0acf-413071fbb9f3>
2012-10-28 02:34:59 +0000

    Reverting back to revision 72 because r73 was not a clean submit

--- trunk/leaves/utils/confusion_matrix.m
+++ trunk/leaves/utils/confusion_matrix.m
@@ -1,18 +1,12 @@
 %---------------------------------------------------
 %sharat@mit.edu
 %---------------------------------------------------
-function C=confusion_matrix(GY,TY)
-	whos GY, TY
-	lbl = unique(GY);
+function C=confusion_matrix(GY,TY,lbl)
 	C  =zeros(length(lbl));
 	for l=1:length(lbl)
 		idx = find(GY==lbl(l));
 		for m=1:length(lbl)
-			if (isempty(idx))
-				C(l,m) = 0;
-			else
-				C(l,m)=sum(TY(idx)==lbl(m))/length(idx);
-			end;
+			C(l,m)=sum(TY(idx)==lbl(m))/length(idx);
 		end;
 	end;
 %end function
